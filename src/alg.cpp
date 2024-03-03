593ece90f4d0bced1123610bd0faf51d6471586b
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-03 19:30:35 +0300

    Задание 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
+#include <cstdint>
 #include <vector>
-
+#include <cmath>
 bool checkPrime(uint64_t value) {
     if (value <= 1) return false;
     if (value <= 3) return true;
@@ -13,7 +14,6 @@ bool checkPrime(uint64_t value) {
     }
     return true;
 }
-
 uint64_t nPrime(uint64_t n) {
     if (n == 1) return 2;
     if (n == 2) return 3;
@@ -28,7 +28,6 @@ uint64_t nPrime(uint64_t n) {
         candidate += 2;
     }
 }
-
 uint64_t nextPrime(uint64_t value) {
     if (value <= 1) return 2;
     
@@ -40,7 +39,6 @@ uint64_t nextPrime(uint64_t value) {
         candidate++;
     }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
